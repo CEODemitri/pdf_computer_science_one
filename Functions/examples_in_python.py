6a55e8469625f94d36fe28a4d7506ce1d74592c7
ceoDemitri <demichole@gmail.com>
2024-11-06 12:25:40 -0600

    complete loops, working on funtions

---
+++ Functions/examples_in_python.py
@@ -0,0 +1,8 @@
+# A function is a sequence of instructions packaged into a unit that can be reused
+# a function performs a specific task
+
+# basic function
+def greet(name = "World"):
+    print ("Hello " + name)
+
+greet("Hannah")
