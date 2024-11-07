3f7ef9ae9c572ec8ce565670fdfefb5f8c65caa5
ceoDemitri <demichole@gmail.com>
2024-11-07 16:19:15 -0600

    made it to the exercises

--- Functions/examples_in_python.py
+++ Functions/examples_in_python.py
@@ -17,3 +17,4 @@ print(upperCase("hello"))
 # Exercises
 # 5.2 ::Write a fucntion that scales an input x to its scientific notation scale so that 1 ≤ x ≤ 10. amount x is shifted should be stored in a pass by reference parameter. 
 # ex. x = 314.15 should return 3.1415 and n = -2
+
