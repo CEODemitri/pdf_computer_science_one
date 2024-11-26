df5deb6b0ac78d1ff82cefc8b8b60aee4b54768d
ceoDemitri <demichole@gmail.com>
2024-11-26 09:59:06 -0600

    complete notes of collections. starts the exercises

---
+++ Arrays_Collections_DynamicMemory/Exercises.py
@@ -0,0 +1,10 @@
+# write a function to return the index of maximum element in an array of numbers
+
+
+# write a function to return the inde of minimum element in an array of numbers
+
+
+# write a function to compute the mean(average) of an array of numbers
+
+
+# write a fn to cpmpute the standard deviation of an array of numbers
